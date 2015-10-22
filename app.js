87467050e8b450c17103e0f4cadb33e3fdb26c5f
Julian Sinai <jsinai@brightsign.biz>
2015-10-22 17:08:16 +0100

    Bug 22940 - Clock Bulletins Do Not Display - 6.0.41 FW. The difference between webkit and chromium is how we are handling parameters passed to an html page: chromium does not like the # in the url.

--- app.js
+++ app.js
@@ -4,3 +4,10 @@
 
 var bsApp = angular.module('bsApp', []);
 
+
+bsApp.config(function($locationProvider){
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+});
